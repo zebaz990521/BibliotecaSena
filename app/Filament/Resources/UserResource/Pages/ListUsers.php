03c937fb17494c6baf700850db0af09081e30325
zebaz990521 <zebaz.21cas@gmail.com>
2025-06-20 22:15:52 -0500

    Agregando filtros a tabla users

--- app/Filament/Resources/UserResource/Pages/ListUsers.php
+++ app/Filament/Resources/UserResource/Pages/ListUsers.php
@@ -3,7 +3,10 @@
 namespace App\Filament\Resources\UserResource\Pages;
 
 use App\Filament\Resources\UserResource;
+use App\Models\User;
 use Filament\Actions;
+use Filament\Resources\Components\Tab;
+use Illuminate\Database\Eloquent\Builder;
 use Filament\Resources\Pages\ListRecords;
 
 class ListUsers extends ListRecords
@@ -16,5 +19,29 @@ class ListUsers extends ListRecords
             Actions\CreateAction::make()
             ->label("Crear Usuario"),
         ];
+    }
+
+    public function getTabs(): array
+    {
+        return [
+            'todos' => Tab::make()
+                ->icon('heroicon-m-user-group')
+                ->badge(User::query()->count())
+                ->badgeColor('success'),
+            'activos' => Tab::make()
+                ->icon('heroicon-o-user-plus')
+                ->badge(User::query()->where('status', true)->count())
+                ->badgeColor('success')
+                ->modifyQueryUsing(fn (Builder $query) => $query->where('status', true)),
+            'inactivos' => Tab::make()
+                ->modifyQueryUsing(fn (Builder $query) => $query->where('status', false))
+                ->badge(User::query()->where('status', true)->count())
+                ->badgeColor('danger'),
+        ];
+    }
+
+    public function getDefaultActiveTab(): string | int | null
+    {
+        return 'todos';
     }
 }
